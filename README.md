1fc68bf17d3780808e57988079eaa5122922ecac
ayhanoruc <ayhan.orc.2554@gmail.com>
2024-01-14 19:11:19 +0300

    endpoint structure created

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 1. build & configure the environment
 2. install dependencies
 3. create fastapi app + index endpoint + run uvicorn for testing
+4. define empty endpoints
+    ![Alt text](image.png)
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+#linkedin account = Ayhanoruc www.linkedin.com/in/ayhan-oruç-601224184
+app = FastAPI(
+    title="MyNotes API",
+    version="1.0.0",
+    description="MyNotes API with FastAPI using async database connectors",
+    contact={
+        "name": "Ayhan Oruc",
+        "url": "http://www.linkedin.com/in/ayhan-oruç-601224184",
+    }
+)
 
 
 
@@ -9,3 +18,26 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "OK"}
+
+@app.get("/notes")
+def get_all_notes():
+    pass 
+
+@app.get("/notes/{note_id}")
+def get_note_by_id(note_id):
+    pass
+
+@app.post("/notes")
+def create_note():
+    pass
+
+@app.put("/notes/{note_id}")
+def update_note(note_id):
+    pass
+
+@app.delete("/notes/{note_id}")
+def delete_note(note_id):
+    pass
+
+
+
